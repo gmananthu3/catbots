cbbb576ee2848af9426f3c497e3d8e0e788f1d1b
Jack FF <91454775+gmananthu3@users.noreply.github.com>
2021-12-09 23:56:20 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,13 +12,13 @@ if (Config.WORKTYPE == 'private') {
 
     Asena.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '15369524516-1612300121@g.us') {
+        if (message.jid === '120363040192713213@g.us') {
 
             return;
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'CatBot created by *Basil sir*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'Bot created by *JACK FF*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '\n\n---------------------', MessageType.text);
@@ -30,13 +30,13 @@ else if (Config.WORKTYPE == 'public') {
 
     Asena.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '54218542512-1612300121@g.us') {
+        if (message.jid === '918943082947@s.whatsapp.net') {
 
             return;
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'CatBot created by *Basil sir*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'Bot created by *JACK SER*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '*HE IS MY BOSS*', MessageType.text);
